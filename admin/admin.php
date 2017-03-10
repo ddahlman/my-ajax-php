56c06f8abf6f7cfa118e3cacdef5ff220ee764ff
therealDaze <danieldahlman@outlook.com>
2017-03-10 15:13:59 +0100

    merge conflicts also

--- admin/admin.php
+++ admin/admin.php
@@ -1,11 +1,4 @@
 <div class='container'>
-<<<<<<< HEAD
-    <div class='well'>
-        <label for="home-text" class='control-label'>Välkomstext</label>
-        <textarea name='home-text' class='form-control'>
-        </textarea>
-    </div>
-=======
   <div class='well'>
     <h1>Ändra din text</h1>
     <div class='form-group'>
@@ -17,5 +10,4 @@
       <button class='btn btn-danger btn-sm'>ta bort</button>
     </div>
   </div>
->>>>>>> b1c652a36b4051aacb8647ff7e64b543f82334db
 </div>
