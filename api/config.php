7ce2921aef32e1f1a4a88b3825564d2e74dc4dfc
therealDaze <danieldahlman@outlook.com>
2017-03-28 11:01:27 +0200

    fixed so that put requests work

--- api/config.php
+++ api/config.php
@@ -3,6 +3,9 @@ $dbhost = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $db = "ajaxtest";
+header('Content-Type: application/json');
+/*header('Access-Control-Allow-Methods: GET, POST, PUT');*/
 
 $connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $db);
+mysqli_query($connection, "SET NAMES utf8");
 ?>
