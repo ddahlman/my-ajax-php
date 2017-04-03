d270bc96a779b05db76badb7a2d8dbebd550a4dc
therealDaze <danieldahlman@outlook.com>
2017-04-03 11:47:32 +0200

    made some changes

--- admin/admin_users.php
+++ admin/admin_users.php
@@ -3,23 +3,22 @@
     <h1>Ändra din text</h1>
     <div class='form-group'>
       <label class='control-label' for="name">Namn</label>
-      <input class='form-control' name='name' type="text">
+      <input class='form-control' name='name' id='name' type="text">
     </div>
     <div class='form-group'>
       <label class='control-label' for="address">Adress</label>
-      <input class='form-control' name='address' type="text">
+      <input class='form-control' name='address' id='address' type="text">
     </div>
     <div class='form-group'>
       <label class='control-label' for="phone">Telefon</label>
-      <input class='form-control' name='phone' type="text">
+      <input class='form-control' name='phone' id='phone' type="text">
     </div>
     <div class='form-group'>
       <label class='control-label' for="email">E-mail</label>
-      <input class='form-control' name='email' type="text">
+      <input class='form-control' name='email' id='email' type="text">
     </div>
-    <div class='btn-group'>
-      <button class='btn btn-info btn-sm'>spara ändringar</button>
-      <button class='btn btn-danger btn-sm'>ta bort</button>
-    </div>
+    <button class='btn btn-info btn-sm' id='save-user'>spara användare</button>
   </div>
+
+  <div id='admin-users'></div>
 </div>
--- js/admin-ajax.js
+++ js/admin-ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    /*GET text from db-----------------------------------------*/
     $.ajax({
         url: "../api/?/welcome_text",
         success: (result) => {
@@ -8,7 +8,7 @@ $(document).ready(function () {
             $('#txt').html(txt);
         }
     });
-
+    /*PUT update text-------------------------------------------*/
     $('#save-text').on('click', function () {
         var text = $('#txt').val();
 
@@ -23,7 +23,7 @@ $(document).ready(function () {
             }
         });
     });
-
+    /*PUT------------with delete text--------------*/
     $('#delete-text').on('click', function () {
         var text = '';
         $.ajax({
@@ -38,4 +38,40 @@ $(document).ready(function () {
             }
         });
     });
+    /*GET users----------------------------------------*/
+    $.get('../api/?/user')
+        .then((response) => {
+            console.log(response.users);
+            var userinfo = response.users.map((user) => {
+                var info = `<div class='well'>
+                            <ul>
+                            <li>${user.name}</li>
+                            <li>${user.address}</li>
+                            <li>${user.phone}</li>
+                            <li>${user.email}</li>
+                            </ul>
+                            </div>`;
+
+                return info;
+            });
+
+            $('#admin-users').html(userinfo);
+        });
+    $('#save-user').on('click', function () {
+        var jsonarr = [];
+        var info = {
+            name: $('#name').val(),
+            address: $('#adress').val(),
+            phone: $('#phone').val(),
+            email: $('#email').val()
+        };
+        jsonarr.push(info);
+        $.post('../api/?/user', {
+                data: JSON.stringify(jsonarr)
+            })
+            .then((response) => {
+                console.log(response.users);
+            });
+    });
+
 });
